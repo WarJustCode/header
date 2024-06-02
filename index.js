a63cb96064517cccc479bb8e342e26ae130c99e6
warjustcode <152807905+WarJustCode@users.noreply.github.com>
2024-06-02 15:58:13 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+// Brand | Header Responsive | Start //
+
+function Hamburger() {
+
+    const hamburger = document.querySelector(".Hamburger");
+    const ul = document.querySelector(".Nav ul");
+    
+    //
+
+        hamburger.classList.toggle("HeaderHamburgerActive");
+        ul.classList.toggle("HeaderHamburgerActive");
+
+    //
+
+}
+
+// Brand | Header Responsive | End //
